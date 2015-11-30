1f795d304631d3ac3108a69dc16bd8188d661543
Dan Vanderkam <danvdk@gmail.com>
2015-11-30 12:39:11 -0500

    Rename github.py

--- test/github_test.py
+++ test/github_test.py
@@ -1,8 +1,8 @@
-from util import github
+from util import github_util
 
 from nose.tools import *
 
 def test_make_github_url_github_pages():
     remote = 'danvk/danvk.github.io.git'
     eq_('https://github.com/danvk/danvk.github.io',
-        github.make_github_url(remote))
+        github_util.make_github_url(remote))
