96baa198a498e3e5becb6ba071bf5aad9721db3d
Ryan Williams <ryan.blake.williams@gmail.com>
2020-04-19 16:41:30 +0000

    improve tag printing

--- util/tags.py
+++ util/tags.py
@@ -1,18 +1,25 @@
 
-import sys
+from subprocess import check_output
+from sys import exit
 
 from piece import Pieces
 
-import subprocess
+def get_tags(n=None):
+    tags = [
+        tag
+        for tag in
+        check_output([ 'git', 'tag', '-l' ]) \
+        .decode() \
+        .split('\n')
+        if tag
+    ]
+    if n: return tags[:n]
+    return tags
 
-def get_tags():
-    return [tag for tag in subprocess.check_output(['git', 'tag', '-l']).decode().split('\n') if tag]
 
-
-def print_recent_tags():
-    tags = get_tags()
-    if not tags:
-        sys.exit(0)
+def print_recent_tags(n=None):
+    tags = get_tags(n)
+    if not tags: exit(0)
 
     pieces = Pieces()
     pieces.parse_log(["--no-walk"] + tags)
