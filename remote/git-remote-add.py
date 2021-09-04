8bb7aff664900ce02468a55f20f9c79e10899915
Ryan Williams <ryan@runsascoded.com>
2021-09-04 16:50:28 -0400

    `grat`: default to toggling remote b/t SSH/HTTPS

--- remote/git-remote-add.py
+++ remote/git-remote-add.py
@@ -8,15 +8,18 @@ from subprocess import check_call, check_output
 parser = ArgumentParser()
 parser.add_argument('--https',action='store_true',help='When set, add an HTTPS remote')
 parser.add_argument('-F','--no-fetch',action='store_true',help="When set, don't fetch from the newly added remote")
+parser.add_argument('-i','--in-place',action='store_true',help='When set, replace the existing remote')
 parser.add_argument('-s','--ssh',action='store_true',help='When set, add an SSH remote')
 parser.add_argument('remote',nargs='?',help='Name of the remote to add')
 parser.add_argument('name',nargs='?',help='Name of the remote to add')
 args = parser.parse_args()
 https = args.https
 fetch = not args.no_fetch
+in_place = args.in_place
 ssh = args.ssh
 remote = args.remote
 name = args.name
+
 
 if not remote:
     remotes = [ remote.strip() for remote in check_output(['git','remote']).decode().split('\n') ]
@@ -34,24 +37,37 @@ if url.scheme:
         raise ValueError(f'Unrecognized URL: {url_str}')
 
     assert not ssh
-    if not name: name = 's'
 
     ssh_url = f'git@{url.hostname}:{url.path[1:]}'
     if not ssh_url.endswith('.git'): ssh_url += '.git'
 
-    print(f'Adding SSH remote {name}: {ssh_url}')
-    check_call(['git','remote','add',name,ssh_url])
+    if in_place:
+        name = remote
+        print(f'Re-routing HTTPS remote {name}: {url_str} → {ssh_url}')
+        check_call(['git', 'remote', 'set-url', name, ssh_url ])
+    else:
+        if not name:
+            name = 's'
+        print(f'Adding SSH remote {name}: {ssh_url}')
+        check_call(['git','remote','add',name,ssh_url])
 else:
     assert not https
-    if not name: name = 'h'
 
     ssh_url_regex = r'git@(?P<hostname>[^:]+):(?P<path>[\w/\-]+)\.git'
     m = match(ssh_url_regex, url_str).groupdict()
 
     https_url = f'https://{m["hostname"]}/{m["path"]}'
 
-    print(f'Adding HTTPS remote {name}: {https_url}')
-    check_call(['git','remote','add',name,https_url])
+    if in_place:
+        name = remote
+        print(f'Re-routing SSH remote {name}: {url_str} → {https_url}')
+        check_call(['git', 'remote', 'set-url', name, https_url ])
+    else:
+        if not name:
+            name = 'h'
+        print(f'Adding HTTPS remote {name}: {https_url}')
+        check_call(['git','remote','add',name,https_url])
+
 
 if fetch:
     check_call(['git','fetch',name])
