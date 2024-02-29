f0c700e8a5ee4ecc71fbf2ed21bb10ed2e46d878
Ryan Williams <ryan@runsascoded.com>
2024-02-29 08:41:57 -0500

    tag, bisect improvements

--- util/tags.py
+++ util/tags.py
@@ -1,27 +1,35 @@
-
 from subprocess import check_output
-from sys import exit
+from sys import exit, stderr
 
 from piece import Pieces
 
+
 def get_tags(n=None):
+    cmd = [
+        'git', 'tag',
+        '-l',
+        '--format=%(refname:short)',
+        '--sort=-creatordate',
+    ]
+    # stderr.write(f"Running: {' '.join(cmd)}\n")
     tags = [
         tag
-        for tag in
-        check_output([ 'git', 'tag', '-l' ]) \
-        .decode() \
-        .split('\n')
-        if tag
+        for tag in (
+            check_output(cmd)
+            .decode()
+            .split('\n')
+        ) if tag
     ]
-    if n: return tags[:n]
+    if n:
+        tags = tags[:n]
     return tags
 
 
 def print_recent_tags(n=None):
     tags = get_tags(n)
-    if not tags: exit(0)
+    if not tags:
+        exit(0)
 
     pieces = Pieces()
     pieces.parse_log(["--no-walk"] + tags)
     pieces.pretty_print()
-
