c33c323a76b50b5fc5b33c04d366af8fb1d1f00a
Ryan Williams <ryan.williams@tiledb.com>
2024-08-09 12:49:19 -0400

    update hash-files.py

--- github/hash-files.py
+++ github/hash-files.py
@@ -1,38 +1,17 @@
 #!/usr/bin/env python
-from functools import partial
-from sys import stderr
-
-import click
+# Standalone script that mimics GitHub Actions' hashFiles helper
+# See also: https://gist.github.com/ryan-williams/2e616363c68072bf67cad675fcb18f3b
 import hashlib
+import sys
 
 
-err = partial(print, file=stderr)
+sha256 = hashlib.sha256()
 
-
-def compute_sha256(file_path):
-    sha256 = hashlib.sha256()
-    with open(file_path, "rb") as f:
-        # Read and update hash string value in blocks of 4K
+for path in sys.argv[1:]:
+    file_sha256 = hashlib.sha256()
+    with open(path, "rb") as f:
         for byte_block in iter(lambda: f.read(4096), b""):
-            sha256.update(byte_block)
-    return sha256.digest()
+            file_sha256.update(byte_block)
+    sha256.update(file_sha256.digest())
 
-
-@click.command()
-@click.option('-v', '--verbose', is_flag=True, help="Log each file's hexdigest to stderr")
-@click.argument('paths', nargs=-1, type=click.Path(exists=True))
-def main(verbose, paths):
-    sha256 = hashlib.sha256()
-    for path in paths:
-        # Compute sha256 digest of path contents
-        digest = compute_sha256(path)
-        if verbose:
-            hexdigest = digest.hex()
-            err(f"{path}: {hexdigest}")
-        sha256.update(digest)
-    result = sha256.hexdigest()
-    print(result)
-
-
-if __name__ == "__main__":
-    main()
+print(sha256.hexdigest())
