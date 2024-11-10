694120ed417e96523b95c85a0830a50cd7fb7f30
Ryan Williams <ryan@runsascoded.com>
2024-11-10 11:55:50 -0500

    `count-completions.sh`: normalize newlines

--- scripts/count-completions.sh
+++ scripts/count-completions.sh
@@ -5,4 +5,4 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 docker build -t git-helpers -f test/docker/git-helpers.dockerfile . 2>/dev/null
-docker run --rm -it --name git-helpers git-helpers "$@"
+docker run --rm -it --name git-helpers git-helpers "$@" | tr -d '\r'
