95b8aca75691f167bbd886ec5e6f2c21bb144ce4
Ryan Williams <ryan@runsascoded.com>
2025-01-25 18:32:05 -0500

    `clone-and-source.sh` improvements

--- clone/clone-and-source.sh
+++ clone/clone-and-source.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a ARGS
+declare -a args
 
 branch=
 inplace=
@@ -25,7 +25,7 @@ do
       esac
     done
     shift $((OPTIND-1))
-    ARGS+=("$1")
+    args+=("$1")
     shift
 done
 
@@ -44,32 +44,30 @@ EOF
   exit 1
 }
 
-nargs=${#ARGS[@]}
-echo "Found $nargs positional args"
-if [ $nargs -eq 0 ]; then
+set -- "${args[@]}"
+echo "Found $# positional args" >&2
+if [ $# -eq 0 ]; then
   usage "No positional args found"
 else
-  url="${ARGS[0]}"
-  ARGS=("${ARGS[@]:1}")
+  url="$1"; shift
   if [ "${url#git@}" != "$url" ] || [ "${url#https://}" != "$url" ]; then
     # $url appears to be a full git URL already
     :
   else
-    # otherwise, assume github.com and HTTPS protocol
+    # Otherwise, assume github.com and HTTPS protocol
     url="https://github.com/$url"
   fi
-  if [ $nargs -eq 2 ]; then
-    dest="${ARGS[0]}"
-    ARGS=("${ARGS[@]:1}")
-  elif [ $nargs -gt 2 ]; then
-    usage "Too many positional args"
+  if [ $# -eq 2 ]; then
+    dest="$1"; shift
+  elif [ $# -gt 2 ]; then
+    usage "Too many positional args ($# extra)"
   fi
 fi
 
 if [ -z "$inplace" ]; then
   dir="${url%.git}"
   dir="${dir##*/}"
-  echo "Cloning $url to $dir"
+  echo "Cloning $url to $dir" >&2
   if [ -z "$dest" ]; then
     git clone -j "$jobs" "$url"
   else
@@ -78,7 +76,7 @@ if [ -z "$inplace" ]; then
 
   pushd "$dir" || exit 2
   if [ -n "$branch" ]; then
-    echo "Checking out branch $branch"
+    echo "Checking out branch $branch" >&2
     git checkout "$branch"
   fi
   git submodule update --init --recursive --jobs "$jobs" || return 100 2>/dev/null || exit 100
@@ -88,17 +86,17 @@ if [ -z "$inplace" ]; then
     script=".*rc"
   fi
 else
-  echo "\"Cloning\" $url in-place"
+  echo "\"Cloning\" $url in-place" >&2
   git init
   remote="upstream"
-  git remote add "$remote" "$url" || echo "Found existing remote \"$remote\""
+  git remote add "$remote" "$url" || echo "Found existing remote \"$remote\"" >&2
   git fetch "$remote" --recurse-submodules
 
   # Helper for backing up any files already present that would conflict with the git checkout (and cause it to abort)
   bak() {
     for arg in "$@"; do
       if [ -e "$arg" ]; then
-        echo "Backing up $arg to $arg.bak"
+        echo "Backing up $arg to $arg.bak" >&2
         mv "$arg" "$arg.bak"
       fi
     done
@@ -110,14 +108,18 @@ else
   git ls-tree -r -z --name-only "$remote/$branch" | xargs -0 bash -c 'bak "$@"' _
   unset bak
 
-  echo "Checking out branch $branch"
+  echo "Checking out branch $branch" >&2
   git checkout "$branch"
   git submodule update --init --recursive --jobs "$jobs"
 fi
 
 if [ -n "$script" ]; then
-  echo "Appending \"source $PWD/$dir/$script\" to .bashrc"
-  echo "source $PWD/$dir/$script" >> ~/.bashrc
+  cmd="for f in \"$PWD/$dir\"/$script; do . \$f; done"
+  echo "Appending '$cmd' to .bashrc" >&2
+  echo "$cmd" >> ~/.bashrc
+  cmd="export PATH=\"$PWD/$dir:\$PATH\""
+  echo "Appending '$cmd' to .bashrc" >&2
+  echo "$cmd" >> ~/.bashrc
 fi
 
-. ~/.bashrc
+. "$HOME/.bashrc"
