53e641d59c2eae613b299937c17508f22ddabd63
Ryan Williams <ryan@runsascoded.com>
2025-01-26 09:56:47 -0500

    `count-completions`/`diff-compgens` improvements

--- scripts/count-completions.sh
+++ scripts/count-completions.sh
@@ -4,5 +4,24 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-docker build -t git-helpers -f test/docker/git-helpers.dockerfile . 2>/dev/null
-docker run --rm -it --name git-helpers git-helpers "$@" | tr -d '\r'
+args=()
+keep_platform=
+rm=(--rm)
+while getopts "ckpv" opt; do
+  case "$opt" in
+    c) args+=(-c) ;;
+    k) rm=() ;;
+    p) keep_platform=1 ;;
+    v) args+=(-v) ;;
+    \?) usage ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ -z "$keep_platform" ] && [ -n "$DOCKER_DEFAULT_PLATFORM" ]; then
+  echo "Unsetting \$DOCKER_DEFAULT_PLATFORM=$DOCKER_DEFAULT_PLATFORM"
+  unset DOCKER_DEFAULT_PLATFORM
+fi
+
+docker build -t git-helpers -f test/docker/git-helpers.dockerfile .
+docker run "${rm[@]}" -it --name git-helpers git-helpers "${args[@]}" "$@" | tr -d '\r'
